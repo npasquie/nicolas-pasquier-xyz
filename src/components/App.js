d262cb9e03dbe5ce484caed13f2d4690749d4426
npasquie <nicpas@live.fr>
2021-10-21 14:41:41 +0200

    navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
-import { Container, Row, Col, Image } from "react-bootstrap"
+import { Container, Row, Col, Image, Button, Navbar } from "react-bootstrap"
 import meImg from "../assets/me.png"
 import "./styles.css"
 
 export default function App(){
 	return(<>
+		<Navbar bg="dark" variant="dark">
+			<Navbar.Brand className="navBarTitle">nicolas.pasquier.xyz</Navbar.Brand>
+		</Navbar>
 		<Container className="main">
 			<Row>
 				<Col sm={8} className="titlePart">
 					<h1>Bring your Blockchain idea to life</h1>
 					<p>I'm a French Blockchain Engineer<br/>
 					I do <div className="important">Fullstack Dapp development, from Smart Contract to WebApp</div></p>
+					<Button className="callButton" variant="dark" size="lg">Schedule a Call</Button>
 				</Col>
 				<Col className="titlePart">
 					<Image className="meImg" src={meImg}/>
